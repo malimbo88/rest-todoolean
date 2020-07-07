26ec8f9cd0feef868c7d567862eed87fe85dae05
Nicola <nicolavalente88@gmail.com>
2020-07-07 17:20:13 +0200

    Prova 3

--- js/script.js
+++ js/script.js
@@ -20,6 +20,14 @@ $(document).ready(function() {
       method: "GET",
       success: function (dataSuccess) {
         todosPrint (dataSuccess)
+
+        //Click Event
+        $(document).on("click", "#btn_create", function() {
+          var inputText = $("#input_text").val()
+          todosWrite (baseUrl, inputText, dataSuccess)
+        });
+        //end Click Event
+
       },
       error: function () {
         alert("Error")
@@ -28,14 +36,50 @@ $(document).ready(function() {
   );
   //end ajax
 
+  //Function todosPrint
+  //Argomento: - array che viene fornito in risposta a chiamata Api todos
+  //Stampa con Handlebars il valore della chiave text di ogni oggetto contenuto in array
+  //Stampa con Handlebars il numero di ogni oggetto stampato in una lista che corrisponde all'indice array +1
   function todosPrint (dataArray) {
     var source = $("#todos-template").html();
     var template = Handlebars.compile(source);
 
     for (var count = 0; count < dataArray.length; count++) {
-      var html = template(dataArray[count]);
+      var listOrderNumber = count + 1;
+      var textString = dataArray[count].text
+      var context = { text: textString, number: listOrderNumber };
+      var html = template(context);
       $(".todos_list").append(html)
     }
   }
+  //end Function todosPrint
+
+  //Function todosWrite
+  function todosWrite (apiUrl, inputVal, dataArray) {
+    //ajax
+    $.ajax(
+      {
+        url: apiUrl,
+        method: "POST",
+        data: { text: inputVal },
+        success: function() {
+          resetHtml ($(".todos_list"))
+          todosPrint (dataArray)
+        },
+        error: function() {
+          alert("error");
+        }
+      }
+    );
+    //end ajax
+  }
+  //end Function todosWrite
+
+  //Function ResetHtml
+  function resetHtml (htmlElement) {
+    htmlElement.html("")
+  }
+  //end Function ResetHtml
+
 });
 //end jquery
